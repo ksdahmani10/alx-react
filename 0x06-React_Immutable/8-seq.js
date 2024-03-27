fe99a826c909ab7f412902dbc4f8c2362d112ab7
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-03-27 14:31:13 +0100

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,21 @@
-import immutable from 'immutable';
+import { Seq } from 'immutable';
 
-const { Seq } = immutable;
+function capitalize(str) {
+  return str.trim().replace(/^\w/, (c) => c.toUpperCase());
+}
 
-export default function printBestStudents() {
-    
+export default function printBestStudents(grades) {
+  const students = Seq(grades);
+
+  console.log(students
+    .filter((student) => student.score > 70)
+    .map((student) => {
+      const { firstName, lastName } = student;
+      return {
+        ...student,
+        firstName: capitalize(firstName),
+        lastName: capitalize(lastName),
+      };
+    })
+    .toJS());
 }
